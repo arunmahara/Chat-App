7e02a36ff2b5f39452f9c848ab715346a39cf285
arunmahara <arunmahara124@gmail.com>
2022-08-15 10:45:21 +0545

    message received from client and saved

--- CHATTY/asgi.py
+++ CHATTY/asgi.py
@@ -12,11 +12,12 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
+from channels.auth import AuthMiddlewareStack 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CHATTY.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket':URLRouter(chat.routing.websocket_urlpatterns)
+    'websocket':AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
 
 })
--- chat/consumers.py
+++ chat/consumers.py
@@ -14,16 +14,49 @@ class MyWebsocketConsumer(WebsocketConsumer):
     def connect(self):
         print('Websocket Connected...')
 
+        self.room_name = self.scope['url_route']['kwargs']['roomName']
+        #add channel to a new or existing group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name,
+            self.channel_name
+            )
 
         self.accept() #to accept connection continuously
+        # self.close() #to reject connection immediately after connection 
 
     
     # this handler is called when data is received form client
     def receive(self, text_data=None, bytes_data=None):
-        pass
+        print('Message Received from client...', text_data)
+
+        data = json.loads(text_data)   #converting string (json) data to python data to save message in database
+        print(data)
+        message = data['msg']
+        room = Room.objects.get(name=self.room_name)
+        if self.scope['user'].is_authenticated:  #user authentication
+
+            data['user'] = self.scope['user'].username  #get user 
+            user = data['user']
+            username = User.objects.get(username = user)
+
+            chat = Chat(room = room, user=username, message = message )
+            chat.save()  # save data in data base for sync consumer
+
+            async_to_sync(self.channel_layer.group_send)(self.room_name,{      # to send message to client form server 
+                'type': 'chat.message',
+                'message': message,
+                'user':  user
+            })
+
+
 
     # this handler is called when either connection to a client is lost , either form the client closing the connection, the server closing the connection or lost of the socket
     def disconnect(self, close_code):
         print('Websocket Disconnected...', close_code) 
+
+         # disconnect channel group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_name, self.channel_name
+        )
 
         raise StopConsumer() 
